a76acb60c725258f3409ab359ed6b7963988c005
natalie <n.l.eskinazi@gmail.com>
2023-05-23 19:04:45 +0100

    Refactor errors for inputs (#326)

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,3 +1,4 @@
+use frost_ed25519::Error;
 use std::io::BufRead;
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -6,49 +7,44 @@ pub struct Config {
     pub max_signers: u16,
 }
 
-pub fn validate_inputs(config: &Config) -> Result<(), frost_ed25519::Error> {
+pub fn validate_inputs(config: &Config) -> Result<(), Error> {
     if config.min_signers < 2 {
-        return Err(frost_ed25519::Error::InvalidMinSigners);
+        return Err(Error::InvalidMinSigners);
     }
 
     if config.max_signers < 2 {
-        return Err(frost_ed25519::Error::InvalidMaxSigners);
+        return Err(Error::InvalidMaxSigners);
     }
 
     if config.min_signers > config.max_signers {
-        return Err(frost_ed25519::Error::InvalidMinSigners);
+        return Err(Error::InvalidMinSigners);
     }
 
     Ok(())
 }
 
-pub fn request_inputs(input: &mut impl BufRead) -> Result<Config, frost_ed25519::Error> {
+pub fn request_inputs(input: &mut impl BufRead) -> Result<Config, Error> {
     println!("The minimum number of signers: (2 or more)");
 
     let mut min = String::new();
     input.read_line(&mut min).unwrap();
 
-    let min_signers = min.trim().parse::<u16>();
-    match min_signers {
-        Ok(_) => (),
-        Err(_) => {
-            return Err(frost_ed25519::Error::InvalidMinSigners);
-        }
-    }
+    let min_signers = min
+        .trim()
+        .parse::<u16>()
+        .map_err(|_| Error::InvalidMinSigners)?;
 
     println!("The maximum number of signers: ");
 
     let mut max = String::new();
     input.read_line(&mut max).unwrap();
-    let max_signers = max.trim().parse::<u16>();
-    match max_signers {
-        Ok(_) => (),
-        Err(_) => {
-            return Err(frost_ed25519::Error::InvalidMaxSigners);
-        }
-    }
+    let max_signers = max
+        .trim()
+        .parse::<u16>()
+        .map_err(|_| Error::InvalidMaxSigners)?;
+
     Ok(Config {
-        min_signers: min_signers.unwrap(),
-        max_signers: max_signers.unwrap(),
+        min_signers,
+        max_signers,
     })
 }
