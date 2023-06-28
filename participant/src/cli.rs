1d52e92ee564d487af4095a771e51574d761531f
natalie <n.l.eskinazi@gmail.com>
2023-06-28 23:12:15 +0100

    Generate nonces and commitments in participant demo (#38)
    * Generate nonces and commitments in participant demo (#20)
    
    * Add comment stating that nonces are secret (#20)

--- participant/src/cli.rs
+++ participant/src/cli.rs
@@ -1,8 +1,15 @@
-use participant::{generate_key_package, request_inputs, Logger};
+use frost::round1;
+use frost_ed25519 as frost;
+use participant::{generate_key_package, print_values, request_inputs, Logger};
+use rand::thread_rng;
 use std::io::BufRead;
 
 pub fn cli(input: &mut impl BufRead, logger: &mut dyn Logger) {
     let config = request_inputs(input, logger).unwrap(); // TODO: handle error
-    let _key_package = generate_key_package(config);
+    let _key_package = generate_key_package(&config);
     logger.log("Key Package succesfully created.".to_string());
+
+    let mut rng = thread_rng();
+    let (nonces, commitments) = round1::commit(config.identifier, &config.signing_share, &mut rng);
+    print_values(nonces, commitments, logger);
 }
--- participant/src/tests/cli.rs
+++ participant/src/tests/cli.rs
@@ -11,7 +11,7 @@ impl Logger for TestLogger {
 
 #[test]
 fn check_cli() {
-    let identifier = "1";
+    let identifier: u16 = 1;
     let pub_key = "470f53fb724502bf5b851471e9f8317616fcc7be9405ccff3347c232a3052ce7";
     let group_pub_key = "42ae1baa1bce5a38c130e60aade154ec8775076e729881aba66dabd0c0ac6332";
     let signing_share = "1edfa2ebd280cba9a72f0bc027d21c30078c11f92e0c908addb958062c1ac900";
@@ -25,19 +25,18 @@ fn check_cli() {
     let mut test_logger = TestLogger(Vec::new());
     cli(&mut reader, &mut test_logger);
 
-    assert_eq!(
-        test_logger.0[0],
-        format!("Your identifier (this should be an integer between 1 and 65535):")
-    );
-    assert_eq!(test_logger.0[1], format!("Your public key:"));
-    assert_eq!(test_logger.0[2], format!("The group public key:"));
-    assert_eq!(test_logger.0[3], format!("Your secret share:"));
-    assert_eq!(
-        test_logger.0[4],
-        format!("Your verifiable secret sharing commitment:")
-    );
-    assert_eq!(
-        test_logger.0[5],
-        format!("Key Package succesfully created.")
-    )
+    // We aren't testing randomness so we are not testing the generation of the nonces and commitments at the top level.
+    // TODO: mock the round1::commit function. To be improved in a later issue.
+
+    let log = [
+        "Your identifier (this should be an integer between 1 and 65535):".to_string(),
+        "Your public key:".to_string(),
+        "The group public key:".to_string(),
+        "Your secret share:".to_string(),
+        "Your verifiable secret sharing commitment:".to_string(),
+        "Key Package succesfully created.".to_string(),
+        "=== Round 1 ===".to_string(),
+    ];
+
+    assert_eq!(test_logger.0[0..=6], log)
 }
