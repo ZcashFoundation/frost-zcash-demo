fcb26f13eacbeef59b4eb3d8e93b7b7f64edde4f
natalie <n.l.eskinazi@gmail.com>
2023-07-10 09:40:43 +0100

    Add exit codes for participant demo (#11)

--- participant/src/cli.rs
+++ participant/src/cli.rs
@@ -1,4 +1,4 @@
-use frost::round1;
+use frost::{round1, Error};
 use frost_ed25519 as frost;
 use participant::round1::{generate_key_package, print_values, request_inputs};
 use participant::round2::{generate_signature, print_values_round_2, round_2_request_inputs};
@@ -6,24 +6,76 @@ use participant::Logger;
 use rand::thread_rng;
 use std::io::BufRead;
 
-pub fn cli(input: &mut impl BufRead, logger: &mut dyn Logger) {
-    let round_1_config = request_inputs(input, logger).unwrap(); // TODO: handle error
-    let key_package = generate_key_package(&round_1_config).unwrap();
+#[derive(PartialEq)]
+pub enum CliError {
+    Config,
+    Signing,
+    KeyPackage,
+}
+
+pub struct ParticipantError {
+    pub frost_error: Error,
+    pub cli_error: CliError,
+}
+
+// This is a little messy because of the use of unwrap(). This can be improved.
+pub fn cli(input: &mut impl BufRead, logger: &mut dyn Logger) -> Result<(), ParticipantError> {
+    let round_1_config = request_inputs(input, logger);
+
+    if let Err(e) = round_1_config {
+        return Err(ParticipantError {
+            frost_error: e,
+            cli_error: CliError::Config,
+        });
+    }
+
+    let round_1_config_ok = round_1_config.unwrap();
+
+    let key_package = generate_key_package(&round_1_config_ok);
+
+    if let Err(e) = key_package {
+        return Err(ParticipantError {
+            frost_error: e,
+            cli_error: CliError::KeyPackage,
+        });
+    }
+
+    let key_package_ok = key_package.unwrap();
+
     logger.log("Key Package succesfully created.".to_string());
 
     let mut rng = thread_rng();
     let (nonces, commitments) = round1::commit(
-        round_1_config.identifier,
-        &round_1_config.signing_share,
+        round_1_config_ok.identifier,
+        &round_1_config_ok.signing_share,
         &mut rng,
     );
+
     print_values(&nonces, commitments, logger);
 
-    let round_2_config = round_2_request_inputs(commitments, input, logger).unwrap(); // TODO: handle errors
+    let round_2_config = round_2_request_inputs(commitments, input, logger); // TODO: handle errors
+
+    if let Err(e) = round_2_config {
+        return Err(ParticipantError {
+            frost_error: e,
+            cli_error: CliError::Config,
+        });
+    }
+
+    let round_2_config_ok = round_2_config.unwrap();
 
     // Sign
 
-    let signature = generate_signature(round_2_config, &key_package, &nonces).unwrap(); // TODO: handle errors
+    let signature = generate_signature(round_2_config_ok, &key_package_ok, &nonces); // TODO: handle errors
 
-    print_values_round_2(signature, logger);
+    if let Err(e) = signature {
+        return Err(ParticipantError {
+            frost_error: e,
+            cli_error: CliError::Signing,
+        });
+    }
+
+    print_values_round_2(signature.unwrap(), logger);
+
+    Ok(())
 }
