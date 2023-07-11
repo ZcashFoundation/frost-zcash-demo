4481c19dfc69b00f02b08dcaf08d28dc5889c52b
natalie <n.l.eskinazi@gmail.com>
2023-07-11 16:21:39 +0100

    Add exit codes for participant demo (#11) (#45)

--- participant/src/main.rs
+++ participant/src/main.rs
@@ -2,7 +2,7 @@ mod cli;
 #[cfg(test)]
 mod tests;
 
-use cli::cli;
+use cli::{cli, CliError};
 use participant::Logger;
 
 use std::io;
@@ -10,7 +10,20 @@ use std::io;
 fn main() -> io::Result<()> {
     let mut reader = Box::new(io::stdin().lock());
     let mut logger = ConsoleLogger::default();
-    cli(&mut reader, &mut logger);
+    let out = cli(&mut reader, &mut logger);
+
+    if let Err(e) = out {
+        if e.cli_error == CliError::Config {
+            {
+                eprintln!("Error: {}", e.frost_error);
+                std::process::exit(exitcode::DATAERR)
+            };
+        };
+        if e.cli_error == CliError::Signing {
+            eprintln!("Error: {}", e.frost_error);
+            std::process::exit(1)
+        };
+    }
 
     Ok(())
 }
