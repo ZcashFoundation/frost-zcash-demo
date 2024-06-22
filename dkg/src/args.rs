349b035cc66158eff4d531027c02b89ae36e726c
Conrado Gouvea <conradoplg@gmail.com>
2024-06-21 21:36:31 -0300

    add missing file

---
+++ dkg/src/args.rs
@@ -0,0 +1,8 @@
+use clap::Parser;
+
+#[derive(Parser, Debug, Default)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short = 'C', long, default_value = "ed25519")]
+    pub ciphersuite: String,
+}
