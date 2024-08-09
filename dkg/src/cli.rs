743bb30282cef54ffceab524f452ffa67ce871d0
Conrado Gouvea <conradoplg@gmail.com>
2024-08-09 20:59:03 -0300

    convert redpallas keys into even-Y format

--- dkg/src/cli.rs
+++ dkg/src/cli.rs
@@ -1,12 +1,44 @@
+use frost_core::keys::{KeyPackage, PublicKeyPackage};
 use frost_core::{self as frost, Ciphersuite};
 
 use rand::thread_rng;
+use reddsa::frost::redpallas::keys::EvenY;
 use std::collections::BTreeMap;
 use std::io::{BufRead, Write};
 
 use crate::inputs::{read_round1_package, read_round2_package, request_inputs};
 
-pub fn cli<C: Ciphersuite + 'static>(
+// The redpallas ciphersuite requires ensuring public key have an even Y
+// coordinate. Since the code uses generics, this trait is used to convert if
+// needed depending on the ciphersuite.
+//
+// If you are adding a new ciphersuite to this tool which does note require
+// this, just implement it and the default implementation (which does nothing)
+// will suffice. See below.
+pub trait MaybeIntoEvenY: Ciphersuite {
+    fn into_even_y(
+        key_packages: (KeyPackage<Self>, PublicKeyPackage<Self>),
+    ) -> (KeyPackage<Self>, PublicKeyPackage<Self>) {
+        key_packages
+    }
+}
+
+// A ciphersuite that does not need the conversion.
+impl MaybeIntoEvenY for frost_ed25519::Ed25519Sha512 {}
+
+impl MaybeIntoEvenY for reddsa::frost::redpallas::PallasBlake2b512 {
+    fn into_even_y(
+        (key_package, public_key_package): (KeyPackage<Self>, PublicKeyPackage<Self>),
+    ) -> (KeyPackage<Self>, PublicKeyPackage<Self>) {
+        let is_even = public_key_package.has_even_y();
+        (
+            key_package.into_even_y(Some(is_even)),
+            public_key_package.into_even_y(Some(is_even)),
+        )
+    }
+}
+
+pub fn cli<C: Ciphersuite + 'static + MaybeIntoEvenY>(
     reader: &mut impl BufRead,
     logger: &mut impl Write,
 ) -> Result<(), Box<dyn std::error::Error>> {
@@ -75,11 +107,11 @@ pub fn cli<C: Ciphersuite + 'static>(
 
     writeln!(logger, "=== DKG FINISHED ===")?;
 
-    let (key_package, public_key_package) = frost::keys::dkg::part3(
+    let (key_package, public_key_package) = MaybeIntoEvenY::into_even_y(frost::keys::dkg::part3(
         &round2_secret_package,
         &received_round1_packages,
         &received_round2_packages,
-    )?;
+    )?);
 
     writeln!(
         logger,
