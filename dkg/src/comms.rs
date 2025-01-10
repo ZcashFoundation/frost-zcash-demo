f1cc6cb43969f1316690c7858c4a8cfbfdcdcadf
Conrado Gouvea <conradoplg@gmail.com>
2025-01-10 18:28:27 -0300

    frost-client: add broadcast channel to DKG

--- dkg/src/comms.rs
+++ dkg/src/comms.rs
@@ -19,12 +19,16 @@ use frost::Identifier;
 
 #[async_trait(?Send)]
 pub trait Comms<C: Ciphersuite> {
+    /// Return this participant's identifier (in case it's derived from other
+    /// information) and the number of participants in the signing session.
     async fn get_identifier(
         &mut self,
         input: &mut dyn BufRead,
         output: &mut dyn Write,
     ) -> Result<(Identifier<C>, u16), Box<dyn Error>>;
 
+    /// Send the Round 1 package to other participants (using echo broadcast),
+    /// and receive their Round 1 packages.
     async fn get_round1_packages(
         &mut self,
         input: &mut dyn BufRead,
@@ -32,6 +36,8 @@ pub trait Comms<C: Ciphersuite> {
         round1_package: round1::Package<C>,
     ) -> Result<BTreeMap<Identifier<C>, round1::Package<C>>, Box<dyn Error>>;
 
+    /// Send the Round 2 packages to other participants, and receive their Round
+    /// 2 packages.
     async fn get_round2_packages(
         &mut self,
         input: &mut dyn BufRead,
@@ -39,5 +45,6 @@ pub trait Comms<C: Ciphersuite> {
         round2_packages: BTreeMap<Identifier<C>, round2::Package<C>>,
     ) -> Result<BTreeMap<Identifier<C>, round2::Package<C>>, Box<dyn Error>>;
 
+    /// Return the map of public keys to identifiers for all participants.
     fn get_pubkey_identifier_map(&self) -> Result<HashMap<Vec<u8>, Identifier<C>>, Box<dyn Error>>;
 }
